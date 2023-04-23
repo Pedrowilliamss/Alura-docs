922ebebd3097561141bd66a3ef2ba5fac03f429c
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-04-23 15:38:48 -0300

    adicionando seções

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -2,12 +2,16 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
-function emitirTextoEditor(texto) {
-    socket.emit("texto_editor", texto);
+function selecionarDocumento(nome) {
+    socket.emit("selecionar_documento", nome)
+}
+
+function emitirTextoEditor(dados) {
+    socket.emit("texto_editor", dados);
 }
 
 socket.on("texto_editor_clientes", (texto) => {
     atualizaTextoEditor(texto);
 })
 
-export { emitirTextoEditor }
+export { emitirTextoEditor, selecionarDocumento } 
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,14 @@
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
-    console.log("Um cliente se conectou! ID:", socket.id)
+    console.log("Um cliente se conectou! ID:", socket.id);
     
-    
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto)
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        socket.join(nomeDocumento);
     });
+    
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+    });
+
 });
