5f9f13c0feb619c7a0aa1085af2c54c374c2febb
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-05-01 08:07:23 -0300

    fix

--- public/cadastro/socket-front-cadastro.js
+++ public/cadastro/socket-front-cadastro.js
@@ -1,11 +1,11 @@
 const socket = io();
 
 function emitirCadastrarUsuario(dados) {
-    socket.emit("cadastrar_usuario",dados);
+  socket.emit("cadastrar_usuario", dados);
 }
 
 socket.on("cadastro_sucesso", () => alert("Cadastro realizado com sucesso!"));
 socket.on("cadastro_erro", () => alert("Erro no cadastro."));
-socket.on("usuario_ja_existente", () => alert("Usuário já existe"));
+socket.on("usuario_ja_existente", () => alert("Usuário já existe!"));
 
-export { emitirCadastrarUsuario }
+export { emitirCadastrarUsuario };
