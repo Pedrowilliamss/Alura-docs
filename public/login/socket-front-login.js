9da982749a2358de6dc3406daddf67c2fb551df0
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-05-01 09:42:05 -0300

    autenticando o usuário

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -4,4 +4,11 @@ function emitirAutenticarUsuario(dados) {
     socket.emit("autenticar_usuario", dados);
 }
 
+socket.on("autenticacao_sucesso", () => {
+    alert("Usuario autenticado com sucesso!");
+    window.location.href = "/";
+});
+socket.on("autenticacao_erro", () => alert("Erro na autenticação."));
+socket.on("usuario_nao_encontrado", () => alert("Usuario não encontrado"));
+
 export {emitirAutenticarUsuario}
