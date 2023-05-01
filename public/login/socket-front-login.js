584147a9ba006fd42c4796fa8fc5a860a3ea11d1
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-05-01 09:29:59 -0300

    capturando informações do login

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+function emitirAutenticarUsuario(dados) {
+    socket.emit("autenticar_usuario", dados);
+}
+
+export {emitirAutenticarUsuario}
--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,11 +1,13 @@
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
+  registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket,io);
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
-  registrarEventosCadastro(socket, io);
 });
